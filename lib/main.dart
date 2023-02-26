979249b163065f9d6a2b1f021678179b47f84c5e
Sekuloski <sekuloski.bojan18@gmail.com>
2023-02-26 09:44:21 +0100

    Revert "Lab 02"
    This reverts commit 2af700deeea3e9f89375fe93f65b407fecd804b7.

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'models/question.dart';
-import 'widget/answer_button.dart';
-import 'widget/clothing_question.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  void _iWasTapped() {
-    setState(() {
-      if (_questionIndex < questions.length - 1) {
-        _questionIndex += 1;
-      }
-    });
-    print('I was tapped');
-  }
-
-  var questions = [
-    Question(
-      questionText: "Select gender",
-      answers: [
-        'Male',
-        'Female',
-      ],
-    ),
-    Question(
-      questionText: "Select size",
-      answers: [
-        'S',
-        'M',
-        'L',
-      ],
-    ),
-    Question(
-      questionText: "Select clothing type",
-      answers: [
-        'Shirt',
-        'Pants',
-        'Trousers',
-      ],
-    ),
-    Question(
-      questionText: "Select colors",
-      answers: [
-        'White',
-        'Black',
-        'Blue',
-        'Green',
-        'Any',
-      ],
-    ),
-  ];
-  var _questionIndex = 0;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Lab 02",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Lab 02"),
-        ),
-        body: Column(
-          children: [
-            ClothingQuestion(questions[_questionIndex].questionText),
-            ...(questions[_questionIndex].answers).map((answer) {
-              return AnswerButton(_iWasTapped, answer);
-            }),
-          ],
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('191263 Bojan Sekuloski'),
       ),
-    );
+      body: Container(
+        color: Colors.blue[900],
+      ),
+    ));
   }
 }
